81731466d40efcd39d8ef64f20e0fb323fbc2b30
Aadil shah <asadilshah919@gmail.com>
2025-09-16 05:37:42 +0530

    basic pracrice

--- logical-Operators.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter you Name: ")
-age = int(input("Enter your Age: "))
-
-if age > 18 and name == "Aadil":
-    print(f"my name is {name}, and i am {age}, years old")
-elif age > 18 and name == "Shah Ali":
-    print(f"my name is {name}, and i am {age}, years old")
-else:
-    print("Name and age not match")
